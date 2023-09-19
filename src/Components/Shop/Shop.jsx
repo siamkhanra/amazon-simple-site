94d1c1a650351f689978c666699b429df814f6e2
Md. Arafat Hossain Siam <siamkhan.ra@gmail.com>
2023-09-19 14:05:36 +0600

    first commit

---
+++ src/Components/Shop/Shop.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react';
+import './Shop.css'
+import Product from '../Product/Product';
+
+const Shop = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect( () => {
+        fetch('products.json')
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    }, []);
+
+    const handleAddToCart = (product) => {
+        // cart.push(product)
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
+
+
+    return (
+        <div className='shop-container'>
+            <div className="products-container">
+                {
+                    products.map(product => <Product key={product.id} product={product} handleAddToCart={handleAddToCart}></Product>)
+                }
+            </div>
+            <div className="cart-container">
+                <h3>Order Summary</h3>
+                <p>Selected Items: {cart.length}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
