ccecfad10707abefd2c21e92a58b60bf82ec3d78
Md. Arafat Hossain Siam <siamkhan.ra@gmail.com>
2023-09-19 15:06:38 +0600

    added update

---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Cart.css'
+
+const Cart = (props) => {
+    // const cart = props.cart; // option-1
+    const {cart} = props; // option-2
+
+    let total = 0;
+    let totalShipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        totalShipping = totalShipping + product.shipping;
+    }
+    const tax = total*7/100;
+    const grandTotal = total + totalShipping + tax;
+    return (
+        <div className='cart'>
+                <h3>Order Summary</h3>
+                <p>Selected Items: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping: ${totalShipping}</p>
+                <p>Tax: ${tax.toFixed(2)}</p>
+                <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+        </div>
+    );
+};
+
+export default Cart;
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './Shop.css'
 import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
@@ -27,8 +28,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h3>Order Summary</h3>
-                <p>Selected Items: {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
